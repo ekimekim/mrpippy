c3afab3427f7913f49e5b47ee7015c95ff75d855
Mike Lang <mikelang3000@gmail.com>
2015-12-07 12:22:48 +0000

    Have the dumper write to an outfile, replacing it each dump

--- examples/dump_client.py
+++ examples/dump_client.py
@@ -4,18 +4,22 @@ gevent.monkey.patch_all()
 
 import json
 import logging
+import os
 
 from gpippy import Client
 
 
-def main(host, level='INFO', interval='5'):
+def main(host, outfile, level='INFO', interval='5'):
 	interval = float(interval)
 	logging.basicConfig(level=level)
 	client = Client(host, on_update=on_update)
 	while not client.finished.ready():
 		gevent.sleep(interval)
 		if client.pipdata.root:
-			print json.dumps(client.pipdata.root.value, indent=4)
+			if os.path.exists(outfile):
+				os.rename(outfile, '{}.tmp'.format(outfile))
+			with open(outfile, 'w') as f:
+				f.write(json.dumps(client.pipdata.root.value, indent=4) + '\n')
 	client.wait()
 
 
