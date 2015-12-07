7edbe6f7d9bf286273b5239025fb20858d14ceeb
Mike Lang <mikelang3000@gmail.com>
2015-12-07 02:47:29 -0800

    Add a simple test tool

---
+++ examples/dump_client.py
@@ -0,0 +1,28 @@
+
+import gevent.monkey
+gevent.monkey.patch_all()
+
+import json
+import logging
+
+from gpippy import Client
+
+
+def main(host, level='INFO', interval='5'):
+	interval = float(interval)
+	logging.basicConfig(level=level)
+	client = Client(host, on_update=on_update)
+	while not client.finished.ready():
+		gevent.sleep(interval)
+		print json.dumps(client.pipdata.root.value, indent=4)
+	client.wait()
+
+
+def on_update(values):
+	for value in values:
+		logging.info("Updated value: {}".format(value))
+
+
+if __name__ == '__main__':
+	import sys
+	main(sys.argv[1:])
