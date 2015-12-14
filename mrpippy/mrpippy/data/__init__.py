767acefb0350737b172716243cce5a547faafa80
Mike Lang <mikelang3000@gmail.com>
2015-12-13 17:40:29 -0800

    data: Add some imports to __init__

--- mrpippy/mrpippy/data/__init__.py
+++ mrpippy/mrpippy/data/__init__.py
@@ -1,2 +1,4 @@
 """This subpackage contains a high-level interface to the data values
 parsed from the low level protocol"""
+from player import Player
+from inventory import Inventory, Item, ITEM_TYPES, EQUIP_STATES
