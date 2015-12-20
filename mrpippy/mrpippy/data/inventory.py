fa8ed8dc4e37c76960490a8f550a28157bfc363b
Mike Lang <mikelang3000@gmail.com>
2015-12-20 07:51:59 -0800

    mrpippy.data.inventory: Add some code for extracting effects from item info

--- mrpippy/mrpippy/data/inventory.py
+++ mrpippy/mrpippy/data/inventory.py
@@ -1,4 +1,6 @@
 
+from collections import defaultdict
+
 from common import Data
 
 ITEM_TYPES = {
@@ -173,3 +175,50 @@ class Item(Data):
 	@property
 	def stack_id(self):
 		return self.root.value['StackID']
+
+	@property
+	def effects(self):
+		"""Returns a dict {effect name: value}, where name is something like 'Rads', 'HP'
+		Note this is based off the displayed value in the pip boy, eg. something that gives
+		+5 HP for 5 seconds will have value {'HP': 25}.
+		We ignore any values that are intended to be interpreted as a percentage for simplicity.
+		We ignore hidden values like '$wt' and '$val'.
+		We ignore long description items.
+		"""
+		results = defaultdict(lambda: 0)
+		for info in self.root.value['itemCardInfoList']:
+			if info.get('showAsPercent'):
+				continue
+			if info.text.startswith('$'):
+				continue
+			if info.get('showAsDescription'):
+				continue
+			value = info['Value']
+			if info.get('scaleWithDuration'):
+				value *= info['duration']
+			results[info['text']] += value
+
+	@property
+	def effects_text(self):
+		"""Similar to effects, but instead of attempting to build a collection of true values,
+		it attempts to collate strings for display of what the item does. It returns a list of strings
+		such as "STR +1" or "HP +33%" or "Slows time for 10 seconds"
+		"""
+		results = []
+		for info in self.root.value['itemCardInfoList']:
+			if info.text.startswith('$'):
+				continue
+			if info.get('showAsDescription'):
+				if info['text'] != 'HP':
+					# work around for "cures all addictions" items,
+					# which have 12 blank 'HP' entries with description True for some reason
+					results.append(info['text'])
+				continue
+			value = info['Value']
+			if info.get('scaleWithDuration'):
+				value *= info['duration']
+			value = '{:+0f}'.format(value)
+			if info.get('showAsPercent'):
+				value += '%'
+			results.append("{} {}".format(info['text'], value))
+		return results
